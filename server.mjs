4fcb5fafa47708a78a02fc50178ed0b21cdd51d6
Rodrigo Rabello <rodrigo.azevedo.rabello@gmail.com>
2026-03-09 20:56:45 -0300

    main

--- server.mjs
+++ server.mjs
@@ -1,10 +1,43 @@
 import { createServer } from "node:http";
+import { Router } from "./router.mjs";
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
+const router = new Router();
+
+router.get("/", (req, res) => {
+  res.end("Home");
+});
+
+router.get("/contato", (req, res) => {
+  res.end("Contato");
+});
+
+router.get("/produto/notebook", (req, res) => {
+  res.end("Produtos - Notebook");
+});
+
+function postProduto(req, res) {
+  res.end("Notebook Post");
+}
+
+router.post("/produto", postProduto);
+
+const server = createServer(async (req, res) => {
+  const url = new URL(req.url, "http://localhost");
+
+  const chunks = [];
+  for await (const chunk of req) {
+    chunks.push(chunk);
+  }
+  const body = Buffer.concat(chunks).toString("utf-8");
+  const handler = router.find(req.method, url.pathname);
+  if (handler) {
+    handler(req, res);
+  } else {
+    res.statusCode = 404;
+    res.end("Não encontrada");
+  }
 });
 
 server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000/");
+  console.log("Server: http://localhost:3000");
 });
